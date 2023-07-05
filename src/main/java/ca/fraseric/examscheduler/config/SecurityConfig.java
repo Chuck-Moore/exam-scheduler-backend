af4797f83e263e7b46739663177bbfd5826b3c43
Can Toprak Cakmur <85279395+Chuck-Moore@users.noreply.github.com>
2023-07-04 22:10:41 -0700

    Disable cors (#10)

--- src/main/java/ca/fraseric/examscheduler/config/SecurityConfig.java
+++ src/main/java/ca/fraseric/examscheduler/config/SecurityConfig.java
@@ -21,6 +21,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.cors(cors -> cors.disable());
 
     http.oauth2ResourceServer(oauth2 -> oauth2
       .opaqueToken(Customizer.withDefaults()));
--- src/main/java/ca/fraseric/examscheduler/config/WebConfig.java
+++ src/main/java/ca/fraseric/examscheduler/config/WebConfig.java
@@ -1,21 +1,21 @@
-package ca.fraseric.examscheduler.config;
+// package ca.fraseric.examscheduler.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+// @Configuration
+// @EnableWebMvc
+// public class WebConfig implements WebMvcConfigurer {
 
-        @Override
-        public void addCorsMappings(CorsRegistry registry) {
-            registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedOriginPatterns("https://exam-scheduler-frontend-*-fire332.vercel.app/")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-        }
-}
+//         @Override
+//         public void addCorsMappings(CorsRegistry registry) {
+//             registry.addMapping("/**")
+//                 .allowedOrigins("http://localhost:5173")
+//                 .allowedOriginPatterns("https://exam-scheduler-frontend-*-fire332.vercel.app/")
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+//                 .allowedHeaders("*")
+//                 .allowCredentials(true);
+//         }
+// }
