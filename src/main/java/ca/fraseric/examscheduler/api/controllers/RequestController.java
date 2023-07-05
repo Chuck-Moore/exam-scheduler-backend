af8d3b81e8d1493b3c4c208ada29ba2faef38aa3
Can Toprak Cakmur <85279395+Chuck-Moore@users.noreply.github.com>
2023-07-04 22:29:37 -0700

    Allow cors request controller (#11)

--- src/main/java/ca/fraseric/examscheduler/api/controllers/RequestController.java
+++ src/main/java/ca/fraseric/examscheduler/api/controllers/RequestController.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@CrossOrigin()
 @RestController
 public class RequestController {
     private final RequestRepository repo;
