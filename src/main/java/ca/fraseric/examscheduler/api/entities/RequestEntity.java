dac2986fe37d8da53f9b103c3e2d3dbecaf7c709
NDrew6071 <93169639+NDrew6071@users.noreply.github.com>
2023-07-07 07:26:25 -0700

    fix RequestEntity.java (#18)
    Co-authored-by: fire332 <96039230+fire332@users.noreply.github.com>

--- src/main/java/ca/fraseric/examscheduler/api/entities/RequestEntity.java
+++ src/main/java/ca/fraseric/examscheduler/api/entities/RequestEntity.java
@@ -1,14 +1,16 @@
 package ca.fraseric.examscheduler.api.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import org.hibernate.validator.constraints.Range;
 
+import java.time.Duration;
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -27,9 +29,12 @@ public class RequestEntity {
   private String courseCode;
   @NotBlank
   private String instructorId;
-  @Range(min = 0, max = 300)
+  @Range(min = 1, max = 1000)
   private int studentCount;
   @Column(nullable = false)
   @ElementCollection
-  private List<ExamDatePreference> examDatePreferences;
+  private List<ZonedDateTime> isoDatePrefs;
+  @Column(nullable = false)
+  @JsonFormat(pattern = "MILLIS")
+  private Duration milliDuration;
 }
