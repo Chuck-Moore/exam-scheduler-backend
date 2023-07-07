023879842079bd9982cbbafe86780d6b3ba7c80a
fire332 <96039230+fire332@users.noreply.github.com>
2023-07-07 08:59:53 -0700

    fix ExamRequest response types (#19)

--- src/main/java/ca/fraseric/examscheduler/api/entities/RequestEntity.java
+++ src/main/java/ca/fraseric/examscheduler/api/entities/RequestEntity.java
@@ -1,8 +1,8 @@
 package ca.fraseric.examscheduler.api.entities;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +14,7 @@ import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@jakarta.persistence.Entity
+@Entity
 @Table(name = "Request")
 @Getter
 @Setter
@@ -22,7 +22,6 @@ import java.util.UUID;
 public class RequestEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
-  @Column(nullable = false)
   @Setter(AccessLevel.NONE)
   private UUID requestId;
   @NotBlank
@@ -31,10 +30,9 @@ public class RequestEntity {
   private String instructorId;
   @Range(min = 1, max = 1000)
   private int studentCount;
-  @Column(nullable = false)
+  @NotNull
   @ElementCollection
   private List<ZonedDateTime> isoDatePrefs;
-  @Column(nullable = false)
-  @JsonFormat(pattern = "MILLIS")
-  private Duration milliDuration;
+  @NotNull
+  private Duration isoDuration;
 }
--- src/main/java/ca/fraseric/examscheduler/config/WebConfig.java
+++ src/main/java/ca/fraseric/examscheduler/config/WebConfig.java
@@ -2,21 +2,17 @@ package ca.fraseric.examscheduler.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
         @Override
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                 .allowedOrigins("*")
-                // .allowedOrigins("http://localhost:5173")
-                // .allowedOriginPatterns("https://exam-scheduler-frontend-*-fire332.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*");
-                // .allowCredentials(true);
         }
+
 }
