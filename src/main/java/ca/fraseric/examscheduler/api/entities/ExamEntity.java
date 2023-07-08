5fa68a48a946c4361a8ce2ea920d2dc2b772fbe7
NDrew6071 <andrewsr.lam@gmail.com>
2023-07-07 21:03:30 -0700

    update ExamEntity for better JPA annotations
    - changed all occurrences of @Column(nullable = false) into @NotNull

--- src/main/java/ca/fraseric/examscheduler/api/entities/ExamEntity.java
+++ src/main/java/ca/fraseric/examscheduler/api/entities/ExamEntity.java
@@ -20,19 +20,19 @@ import java.util.UUID;
 public class ExamEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(nullable = false)
+    @NotNull
     @Setter(AccessLevel.NONE)
     private UUID examId;
     @NotBlank
-    @Column(unique = true)
+    @NotNull
     private String courseCode;
-    @Column(nullable = false)
+    @NotNull
     @Temporal(TemporalType.DATE)
     private LocalDate date; // how to set valid range, maybe within the term?
-    @Column(nullable = false)
+    @NotNull
     @Temporal(TemporalType.TIME)
     private LocalTime startTime;
-    @Column(nullable = false)
+    @NotNull
     @Temporal(TemporalType.TIME)
     private LocalTime endTime;
     @NotBlank
